1781785469487cdc50fdfd1621bdeb6bb1439b24
seunghoon <seunghoon@seunghoon-PC>
2013-02-18 02:50:31 +0900

    이걸로 개발해

--- src/com/example/icmprj/AllTheSource.java
+++
@@ -1,7 +0,0 @@
-package com.example.icmprj;
-
-public class AllTheSource {
-	public AllTheSource() {
-		
-	}
-}
--- src/com/example/icmprj/mydb/picture.java
+++ src/com/example/icmprj/mydb/picture.java
@@ -3,26 +3,28 @@ package com.example.icmprj.mydb;
 import java.util.ArrayList;
 
 public class picture {
-	static ArrayList<picture> picturelist = new ArrayList<picture>();
-			
-	String _ID ="";
-	String DATE ="";
-	String LATI ="";
-	String LONGI ="";
-	String DATA ="";
-	String BUCKET_NAME ="";
-	String DIARY="";
-	public picture(String id,String date, String lati, String longi, String data, String bucket) {
-		_ID =id;
-		DATE =date;
-		LATI =lati;
-		LONGI =longi;
-		DATA =data;
-		BUCKET_NAME =bucket;
+	public static ArrayList<picture> picturelist = new ArrayList<picture>();
+
+	public String _ID = "";
+	public String DATE = "";
+	public String LATI = "";
+	public String LONGI = "";
+	public String DATA = "";
+	public String BUCKET_NAME = "";
+	public String DIARY = "";
+
+	public picture(String id, String date, String lati, String longi,
+			String data, String bucket) {
+		_ID = id;
+		DATE = date;
+		LATI = lati;
+		LONGI = longi;
+		DATA = data;
+		BUCKET_NAME = bucket;
 	}
-	
+
 	public static ArrayList<picture> getlist() {
-	       return picturelist;
+		return picturelist;
 	}
-	
+
 }
